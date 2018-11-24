8c5236d8a04a7612084982f5bca64a8ee794f155
Pruthwiraj S <pruthwi112@gmail.com>
2018-11-24 23:38:33 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pruthwirajs/newdaynotepad/EditorFragment.java
@@ -0,0 +1,221 @@
+package com.example.pruthwirajs.newdaynotepad;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+
+
+public class EditorFragment extends Fragment {
+    private MainActivity activity;
+    private String name;
+    private View view;
+    private EditText etEditor;
+    private EditText etTitle;
+    private String path;
+    ArrayList<String> textUndoHistory = new ArrayList<String>();
+    ArrayList<Integer> selectionStartUndoHistory = new ArrayList<Integer>();
+    ArrayList<Integer> selectionEndUndoHistory = new ArrayList<Integer>();
+    private Button btUndo;
+    private Button btRedo;
+    private Button btSave;
+    private Button btShare;
+    private long tLastEdit;
+    private String previousText;
+    private int undoHistoryCursor = 0;
+    TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            if (System.currentTimeMillis() - tLastEdit > 1000 ||
+                Math.abs(etEditor.getText().length() - textUndoHistory.get(undoHistoryCursor).toString().length()) > 8 ) {
+                saveUndoRedo();
+            }
+            previousText = new String(charSequence.toString());
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            tLastEdit = System.currentTimeMillis();
+//            activity.toast(lastChar(previousText,charSequence.toString()) + "");
+            if (lastChar(previousText, charSequence.toString()) == ' ') saveUndoRedo();
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            activity.editor_modified = true;
+            save();
+        }
+    };
+
+    private void saveUndoRedo() {
+        if (textUndoHistory.size() > 0 && undoHistoryCursor < textUndoHistory.size() - 1) {
+            for (int c = textUndoHistory.size() - 1; c >= undoHistoryCursor; c--) {
+                textUndoHistory.remove(c);
+                selectionStartUndoHistory.remove(c);
+                selectionEndUndoHistory.remove(c);
+            }
+        }
+        textUndoHistory.add(etEditor.getText().toString());
+        selectionStartUndoHistory.add(etEditor.getSelectionStart());
+        selectionEndUndoHistory.add(etEditor.getSelectionEnd());
+        undoHistoryCursor = textUndoHistory.size() - 1;
+        updateUndoRedoButtons();
+    }
+
+    private void retrieveUndoRedo() {
+        etEditor.removeTextChangedListener(textWatcher);
+        etEditor.setText(textUndoHistory.get(undoHistoryCursor).toString());
+        etEditor.setSelection(
+                selectionStartUndoHistory.get(undoHistoryCursor),
+                selectionStartUndoHistory.get(undoHistoryCursor)
+        );
+        etEditor.addTextChangedListener(textWatcher);
+        updateUndoRedoButtons();
+    }
+
+    private void updateUndoRedoButtons() {
+        btUndo.setEnabled(textUndoHistory.size() > 0 && undoHistoryCursor > 0);
+        btRedo.setEnabled(textUndoHistory.size() > 0 && undoHistoryCursor < textUndoHistory.size() - 1);
+    }
+
+    public char lastChar(String oldStr, String newStr){
+        String shorter, longer;
+//        Log.d("string",oldStr+"");
+//        Log.d("string",newStr+"");
+
+        //find which string is shorter so that we don't end up out of bounds
+        if (oldStr.length()<newStr.length() ) {
+            shorter = oldStr;
+            longer = newStr;
+        } else {
+            shorter = newStr;
+            longer = oldStr;
+        }
+
+        //find the first character that is different and return it
+        for (int i = 0; i < shorter.length(); i++) {
+            if (longer.charAt(i) != shorter.charAt(i)) return longer.charAt(i);
+        }
+
+        //if no characters are different then return the last char of the longer string
+        //this means that the undo will be saved if the last char the user erased is a space
+        Log.d("longer",longer);
+        return(longer.charAt(longer.length()-1));
+    }
+
+    public void onPrepareOptionsMenu(Menu menu) {
+        if (menu != null) {
+            menu.findItem(R.id.action_refresh).setVisible(false);
+            menu.findItem(R.id.action_sort_by_name).setVisible(false);
+            menu.findItem(R.id.action_sort_by_date).setVisible(false);
+            activity.invalidateOptionsMenu();
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        activity = (MainActivity) getActivity();
+        view = inflater.inflate(R.layout.editor_fragment, container, false);
+
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+
+        //Set global action bar to this fragment's actions
+        AppCompatActivity rootActivity = (AppCompatActivity) getActivity();
+        rootActivity.setSupportActionBar(toolbar);
+        rootActivity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        rootActivity.getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        setHasOptionsMenu(true);
+
+        etTitle = (EditText) view.findViewById(R.id.etTitle);
+        path = activity.getOpenedFilePath();
+        name = activity.filer.getFileNameWithoutExtension(path);
+        etTitle.setText(name);
+        etEditor = (EditText) view.findViewById(R.id.etEditor);
+        if (activity.filer.exists(path))
+            etEditor.setText(activity.filer.getStringFromFile(path));
+        etEditor.addTextChangedListener(textWatcher);
+
+        textUndoHistory.clear();
+        selectionStartUndoHistory.clear();
+        selectionEndUndoHistory.clear();
+        View rootView = view.getRootView();
+        btUndo = (Button) rootView.findViewById(R.id.btUndo);
+        btUndo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //due to grouping the last few keystrokes might not have been saved
+                //save them now except if we are in the middle of an undo streak
+                //(we don't want to reset the undo stack on every undo)
+                if (undoHistoryCursor == textUndoHistory.size() - 1) saveUndoRedo();
+                undoHistoryCursor--;
+                retrieveUndoRedo();
+            }
+        });
+        btRedo = (Button) view.findViewById(R.id.btRedo);
+        btRedo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                undoHistoryCursor++;
+                retrieveUndoRedo();
+            }
+        });
+        tLastEdit = System.currentTimeMillis();
+        saveUndoRedo();
+        previousText = (String)etEditor.getText().toString();
+		btShare = (Button) view.findViewById(R.id.btShare);
+		btShare.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				share();
+			}
+		});
+        return view;
+    }
+
+    private String getEditorText() {
+        return etEditor.getText().toString();
+    }
+
+    public void save() {
+        String newName = etTitle.getText().toString();
+        String oldPath = path;
+        boolean rename = (!newName.equals(name));
+        if (rename) {
+            path = activity.listFragment.proposeNewFilePath(newName);
+            name = activity.filer.getFileNameWithoutExtension(path);
+        }
+
+        if (activity.filer.writeToFile(path, getEditorText())) {
+            if (rename)
+                activity.filer.delete(oldPath);
+            activity.editor_modified = false;
+        }
+    }
+
+    private void share() {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, getEditorText());
+        sendIntent.setType("text/plain");
+        startActivity(sendIntent);
+    }
+
+}
---
+++ app/src/main/java/com/example/pruthwirajs/newdaynotepad/MainActivity.java
@@ -0,0 +1,219 @@
+package com.example.pruthwirajs.newdaynotepad;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+    private SharedPreferences sharedPref;
+    private SharedPreferences.Editor sharedPrefEditor;
+    private EditorFragment currentEditorFragment;
+    CoordinatorLayout coordinatorLayoutForSnackBar;
+    ListFragment listFragment = new ListFragment();
+    public Filer filer;
+    public boolean editor_modified;
+
+    private String getDefaultPath() {
+        return getExternalFilesDir(null).getAbsolutePath();
+    }
+
+    public String getPath(){
+        return getPath(true);
+    }
+
+    public String getPath(boolean requestPermissions) {
+        String path;
+
+        if (sharedPref.getString("saveLocation", "internal").equals("internal")){
+            path = getDefaultPath();
+            Log.d("storage:", "internal");
+            Log.d("path:", path);
+        } else {
+            path = sharedPref.getString("PATH", getString(R.string.default_shared_path));
+            if (requestPermissions) requestStoragePermission();
+            Log.d("storage:", "shared");
+            Log.d("path:", path);
+        }
+
+        File file = new File(path);
+
+        if (!file.exists()) {
+            if (file.mkdirs()) {
+                Log.d("createDir", "Successfully created the parent dir:" + file.getName());
+            } else {
+                Log.d("createDir", "Failed to create the parent dir:" + file.getName());
+            }
+        }
+
+        if (!file.canWrite()) {
+            String old_path = path;
+            path = getDefaultPath();
+            toast("The path " + old_path + " was not writable. Falling back to default path: " + path);
+        }
+        return path;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        listFragment.refresh(false);
+    }
+
+    public  boolean requestStoragePermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                Log.v("permission","Permission is granted");
+                return true;
+            } else {
+                Log.v("permission","Permission is revoked");
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            Log.v("permission","Permission is granted");
+            return true;
+        }
+    }
+
+    public void setLastOpenedFilePath(String filePath) {
+        sharedPrefEditor.putString("CURRENT_OPENED_FILE_PATH", filePath);
+        sharedPrefEditor.commit();
+    }
+
+    public String getOpenedFilePath() {
+        return sharedPref.getString("CURRENT_OPENED_FILE_PATH", "");
+    }
+
+    public void toast(String text) {
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    public void makeSnackBar(String text) {
+        Snackbar.make(coordinatorLayoutForSnackBar, text, Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
+
+    private void changeFragment(Fragment f, boolean allowBack) {
+        FragmentManager fragmentManager = getFragmentManager();
+
+        // Prune the stack, so "back" always leads home.
+        if (fragmentManager.getBackStackEntryCount() > 0) {
+            onSupportNavigateUp();
+        }
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment_container, f);
+        if (allowBack) {
+            transaction.addToBackStack(null);
+            transaction.setTransition(FragmentTransaction.TRANSIT_NONE);
+        }
+        transaction.commit();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        coordinatorLayoutForSnackBar = (CoordinatorLayout) findViewById(R.id.co_ordinated_layout_main);
+        sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPrefEditor = sharedPref.edit();
+        filer = new Filer(this);
+        changeFragment(listFragment, false); //Show list of files to setLastOpenedFilePath
+        String last_opened_file_path = getOpenedFilePath();
+        if (filer.exists(last_opened_file_path)) {
+            editFile(last_opened_file_path);
+        } else {
+            setLastOpenedFilePath("");//Clear last opened file path
+        }
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().hide();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void editFile(String filePath) {
+        currentEditorFragment = new EditorFragment();
+        changeFragment(currentEditorFragment, true);
+        setLastOpenedFilePath(filePath);
+        editor_modified = false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_sort_by_name) {
+            listFragment.sort(false);
+            return true;
+        }
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_sort_by_date) {
+            listFragment.sort(true);
+            return true;
+        }
+
+        //noinspection SimplifiableIfStatement
+
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_refresh) {
+            listFragment.refresh();
+            return true;
+        }
+
+
+
+        if (id == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        int count = getFragmentManager().getBackStackEntryCount();
+
+        if (count == 0) {
+            super.onBackPressed();
+        } else {
+            setLastOpenedFilePath("");//Clear last opened file path
+            getFragmentManager().popBackStack();
+        }
+    }
+
+}
